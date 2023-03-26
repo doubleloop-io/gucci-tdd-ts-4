1c2dacff1353e7da2e4c10b8c2aae50559bf3131
Massimo Iacolare <info@massimoiacolare.it>
2023-03-26 18:02:46 +0200

    tweak console-log.interceptor

--- src/01-trivia/test-support/console-log.interceptor.ts
+++ src/01-trivia/test-support/console-log.interceptor.ts
@@ -1,20 +1,21 @@
 import { EOL } from "node:os"
 
 export class ConsoleLogInterceptor {
-    private origLog?: () => void
+    private origLog: (() => void) | undefined
     private _logged = ""
 
     startReceiving() {
         this.origLog = console.log.bind(console)
         console.log = (...args: unknown[]) => {
             this._logged += args.map((a) => `${a}`).join(" ") + EOL
-            // this._logged += args.map((a) => JSON.stringify(a)).join(" ") + EOL
         }
     }
 
     stopReceiving() {
         if (!this.origLog) return
         console.log = this.origLog
+
+        this.origLog = undefined
     }
 
     get logged(): string {
