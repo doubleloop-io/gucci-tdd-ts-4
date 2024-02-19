25adecd10721634cf3f1e4ac3cda9e8e64b7dd95
Massimo Iacolare <info@massimoiacolare.it>
2024-02-19 16:27:28 +0100

    QD: add 'Science category' test

--- src/01-trivia/questions-deck.test.ts
+++ src/01-trivia/questions-deck.test.ts
@@ -8,3 +8,11 @@ test("Pop category", () => {
     expect(deck.currentCategoryFor(4)).toBe("Pop")
     expect(deck.currentCategoryFor(8)).toBe("Pop")
 })
+
+test("Science category", () => {
+    const deck = new QuestionsDeck()
+
+    expect(deck.currentCategoryFor(1)).toBe("Science")
+    expect(deck.currentCategoryFor(5)).toBe("Science")
+    expect(deck.currentCategoryFor(9)).toBe("Science")
+})
