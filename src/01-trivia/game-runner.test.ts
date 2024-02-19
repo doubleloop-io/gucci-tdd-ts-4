c80bda4a0e2923d50faa00da641d66896d1e9224
Massimo Iacolare <info@massimoiacolare.it>
2024-02-19 14:21:34 +0100

    experiment with Prando rng

--- src/01-trivia/game-runner.test.ts
+++ src/01-trivia/game-runner.test.ts
@@ -1,18 +1,33 @@
 import { expect, test, beforeEach, afterEach } from "vitest"
 import { GameRunner } from "./game-runner"
 import { ConsoleLogInterceptor } from "./test-support/console-log.interceptor"
+import Prando from "prando"
 
 let interceptor: ConsoleLogInterceptor
 beforeEach(() => {
-    interceptor = new ConsoleLogInterceptor()
-    interceptor.startReceiving()
+    // interceptor = new ConsoleLogInterceptor()
+    // interceptor.startReceiving()
 })
 afterEach(() => {
-    interceptor.stopReceiving()
+    // interceptor.stopReceiving()
 })
 
 test("run game", () => {
     GameRunner.main()
 
-    console.log(interceptor.logged)
+    // expect(interceptor.logged).toMatchSnapshot()
+})
+
+test("Prando", () => {
+    const rng = new Prando(11)
+
+    const numbers = [
+        rng.nextInt(0, 10),
+        rng.nextInt(0, 10),
+        rng.nextInt(0, 10),
+        rng.nextInt(0, 10),
+        rng.nextInt(0, 10),
+    ]
+
+    expect(numbers).toEqual([5, 7, 4, 8, 2])
 })
