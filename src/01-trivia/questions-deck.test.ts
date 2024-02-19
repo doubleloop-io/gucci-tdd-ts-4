30c7b99318fd6b3ae89889b4a30bd33850093393
Massimo Iacolare <info@massimoiacolare.it>
2024-02-19 16:39:27 +0100

    QD: switch to parametric test for 'currentCategoryFor()'

--- src/01-trivia/questions-deck.test.ts
+++ src/01-trivia/questions-deck.test.ts
@@ -1,18 +1,15 @@
 import { expect, test } from "vitest"
 import { QuestionsDeck } from "./questions-deck"
 
-test("Pop category", () => {
+test.each([
+    ["Pop", [0, 4, 8]],
+    ["Science", [1, 5, 9]],
+    ["Sports", [2, 6, 10]],
+    ["Rock", [3, 7, 11]],
+])("%s category at %s", (category: string, places: number[]) => {
     const deck = new QuestionsDeck()
 
-    expect(deck.currentCategoryFor(0)).toBe("Pop")
-    expect(deck.currentCategoryFor(4)).toBe("Pop")
-    expect(deck.currentCategoryFor(8)).toBe("Pop")
-})
-
-test("Science category", () => {
-    const deck = new QuestionsDeck()
-
-    expect(deck.currentCategoryFor(1)).toBe("Science")
-    expect(deck.currentCategoryFor(5)).toBe("Science")
-    expect(deck.currentCategoryFor(9)).toBe("Science")
+    expect(deck.currentCategoryFor(places[0])).toBe(category)
+    expect(deck.currentCategoryFor(places[1])).toBe(category)
+    expect(deck.currentCategoryFor(places[2])).toBe(category)
 })
